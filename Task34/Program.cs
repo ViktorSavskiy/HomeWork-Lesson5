4edd5ca594aa579b096a2d53e88ad5de0a9b1a1e
Viktor <savskij@bk.ru>
2022-09-03 22:40:53 +0300

    Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+Console.WriteLine(string.Join(",", array));
+int c = 0;
+for (int i = 0; i < array.Length; i++)
+{
+   if (array[i]%2 == 0)
+   {
+    c++;
+   } 
+}
+Console.WriteLine("Количество четных элементов в массиве = " + c);
