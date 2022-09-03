88857e2c91164dace8cc7faae7a227ddeb338534
Viktor <savskij@bk.ru>
2022-09-03 22:56:08 +0300

    Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next();
+}
+Console.WriteLine(string.Join(", ", array));
+int numSum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+   if (i%2 == 1)
+   {
+    numSum += array[i];
+   } 
+}
+Console.WriteLine("Сумма элементов на нечетных позициях " + numSum);
