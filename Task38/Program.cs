6efbb594609d963c2070233ae7cd007e818c9dd6
Viktor <savskij@bk.ru>
2022-09-04 14:09:05 +0300

    Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+double[] array = new double[size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().NextDouble();
+}
+Console.WriteLine(string.Join(", ", array));
+
+double maxNum = array[0];
+double minNum = array[0];
+foreach (double el in array)
+{
+    if (el > maxNum)
+    {
+        maxNum = el;
+    }
+    if (el < minNum)
+    {
+        minNum = el;
+    }
+}
+Console.Write("Разница между максимальным значением массива и минимальным: ");
+Console.WriteLine(maxNum - minNum);
